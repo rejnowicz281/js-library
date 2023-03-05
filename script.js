6fa841c56d22f6dcd4eee1b6b9434a03d7e85603
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-03-05 15:30:17 +0100

    Implement markAsRead and Delete buttons for books

--- script.js
+++ script.js
@@ -16,20 +16,11 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = () => {
-    return (
-      title +
-      " by " +
-      author +
-      ", " +
-      pages +
-      " pages, " +
-      (read ? "already read" : "not read yet")
-    );
-  };
 }
 
-submitBook.addEventListener("click", (event) => {
+let library = [];
+
+submitBook.addEventListener("click", () => {
   const book = new Book(
     bookTitle.value,
     bookAuthor.value,
@@ -37,8 +28,11 @@ submitBook.addEventListener("click", (event) => {
     bookRead.checked
   );
 
+  library.push(book);
+
   let bookContainer = document.createElement("div");
   bookContainer.classList.add("book");
+  bookContainer.setAttribute("id", `book-${library.length - 1}`);
 
   function appendColumn(columnLegend, columnMain) {
     let bookColumn = document.createElement("div");
@@ -49,6 +43,9 @@ submitBook.addEventListener("click", (event) => {
 
     let main = document.createElement("span");
     main.classList.add("column-main");
+    if (columnLegend == "Progress") {
+      main.setAttribute("id", `book-${library.length - 1}-progress`);
+    }
 
     legend.textContent = columnLegend;
     main.textContent = columnMain;
@@ -57,9 +54,50 @@ submitBook.addEventListener("click", (event) => {
     bookContainer.append(bookColumn);
   }
 
-  appendColumn("Title", bookTitle.value);
-  appendColumn("Author", bookAuthor.value);
-  appendColumn("Pages", bookPages.value);
-  appendColumn("Progress", bookRead.checked ? "Already Read" : "Not Yet Read");
+  function appendButtons() {
+    let markAsReadColumn = document.createElement("div");
+    let deleteColumn = document.createElement("div");
+    markAsReadColumn.classList.add("book-column");
+    deleteColumn.classList.add("book-column");
+
+    let markAsRead = document.createElement("button");
+    markAsRead.textContent = book.read ? "Mark as Unread" : "Mark as Read";
+    markAsRead.dataset.bookIndex = library.length - 1;
+    markAsRead.addEventListener("click", function () {
+      if (book.read) {
+        book.read = false;
+        markAsRead.textContent = "Mark as Read";
+        document.getElementById(
+          `book-${markAsRead.dataset.bookIndex}-progress`
+        ).textContent = "Not Yet Read";
+      } else {
+        book.read = true;
+        markAsRead.textContent = "Mark as Unread";
+        document.getElementById(
+          `book-${markAsRead.dataset.bookIndex}-progress`
+        ).textContent = "Already Read";
+      }
+    });
+
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.dataset.bookIndex = library.length - 1;
+    deleteButton.addEventListener("click", function () {
+      booksContainer.removeChild(
+        document.getElementById(`book-${deleteButton.dataset.bookIndex}`)
+      );
+      library.splice(deleteButton.dataset.bookIndex, 1);
+    });
+
+    deleteColumn.append(deleteButton);
+    markAsReadColumn.append(markAsRead);
+    bookContainer.append(markAsReadColumn);
+    bookContainer.append(deleteColumn);
+  }
+  appendColumn("Title", book.title);
+  appendColumn("Author", book.author);
+  appendColumn("Pages", book.pages);
+  appendColumn("Progress", book.read ? "Already Read" : "Not Yet Read");
+  appendButtons();
   booksContainer.append(bookContainer);
 });
