7496b802497eacf5a7c3ef40869beb1043a03b0b
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-06-11 12:50:48 +0200

    Add simple javascript validation

--- main.js
+++ main.js
@@ -9,44 +9,51 @@ const bookPages = document.getElementById("pages");
 const bookRead = document.getElementById("read");
 
 showModal.addEventListener("click", function () {
-  modal.classList.toggle("hidden");
+    modal.classList.toggle("hidden");
 });
 
 class Book {
-  constructor(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-  }
-}
-
-class Library{
-  constructor() {
-    this.books = []
-  }
-
-  addBook(book){
-    book.id = this.books.length
-    this.books.push(book)
-  }
-
-  removeBook(id){
-    for (let i = 0; i < this.books.length; i++) {
-      if (this.books[i].id == id) this.books.splice(i, 1);
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
-  }
 }
 
-let library = new Library()
+class Library {
+    constructor() {
+        this.books = [];
+    }
+
+    addBook(book) {
+        book.id = this.books.length;
+        this.books.push(book);
+    }
+
+    removeBook(id) {
+        for (let i = 0; i < this.books.length; i++) {
+            if (this.books[i].id == id) this.books.splice(i, 1);
+        }
+    }
+}
+
+let library = new Library();
 
 submitBook.addEventListener("click", () => {
-  library.addBook(new Book(
-    bookTitle.value,
-    bookAuthor.value,
-    bookPages.value,
-    bookRead.checked
-  ))
-
-  renderLibraryBook(library, library.books[library.books.length-1])
+    if (
+        bookTitle.validity.valid &&
+        bookAuthor.validity.valid &&
+        bookPages.validity.valid
+    ) {
+        library.addBook(
+            new Book(
+                bookTitle.value,
+                bookAuthor.value,
+                bookPages.value,
+                bookRead.checked
+            )
+        );
+        renderLibraryBook(library, library.books[library.books.length - 1]);
+    }
 });
